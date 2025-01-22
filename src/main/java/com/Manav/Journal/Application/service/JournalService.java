5af6984d50ec0eff69812aa15604854ab60aa8a6
moonlitManu <manavdev202@gmail.com>
2025-01-22 15:47:30 +0530

    done

--- src/main/java/com/Manav/Journal/Application/service/JournalService.java
+++ src/main/java/com/Manav/Journal/Application/service/JournalService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +17,16 @@ import com.Manav.Journal.Application.repo.UserRepo;
 @Service
 public class JournalService {
 
-    @Autowired
-    private JournalRepo repo;
+    
+    private final JournalRepo repo;
+    private  final UserRepo userRepo;
+    public JournalService(JournalRepo repo, UserRepo userRepo){
+        this.repo = repo;
+        this.userRepo = userRepo;
+    }
 
-    @Autowired 
-    private  UserRepo userRepo;
+    
+  
 
   
     public JournalEntry saveEntry(JournalEntry entry){
